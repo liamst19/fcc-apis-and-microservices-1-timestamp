dcce0a97228897c846cbc130b476c05751a37116
Glitch (adjoining-hill-alvarezsaurus) <none>
2020-04-18 06:13:17 +0000

    🏫🌹 Checkpoint ./server.js:15054522/1149

--- server.js
+++ server.js
@@ -24,16 +24,27 @@ app.get("/api/hello", function(req, res) {
 });
 
 app.get("/api/timestamp/:date_string?", (req, res) => {
-  const dStr = req.params.date_string;
+  
+  const dStr = req.params.date_string || '';
   const iso8601 = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
-
-  const d = new Date(iso8601.test(dStr) ? dStr : dStr * 1000) || new Date();
-  console.log({dStr, d})
-  res.json(
-    d.toString() === 'Invalid Date' 
-    ? ({ "error" : "Invalid Date" }) 
-    : ({ unix: d.getTime(), utc: d.toUTCString() }) 
-  );
+console.log(dStr)
+  if(iso8601.test(dStr)){
+    const d1 = new Date(dStr);
+    console.log('iso8601', d1)
+    res.json({ "unix": d1.getTime(), "utc": d1.toUTCString()});
+  } else if(dStr === ''){
+    const d2 = new Date();
+    console.log('empty', d2)
+    res.json({ "unix": d2.getTime(), "utc": d2.toUTCString()});
+  } else if(Number.isInteger(Number(dStr))) {
+    const d3 = new Date(Number(dStr));
+    console.log('timestamp', d3)
+    res.json({ "unix": d3.getTime(), "utc": d3.toUTCString()});
+  }
+  else {
+    console.log('invalid')
+    res.json({ "error" : "Invalid Date" });
+  }
 });
 
 // listen for requests :)
